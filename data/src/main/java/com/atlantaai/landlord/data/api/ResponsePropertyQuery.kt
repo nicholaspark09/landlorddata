d21300b35b552c4b371eed06dcefa9ddc5e82658
Nicholas Park <npark@pplweb.com>
2022-05-12 18:10:18 +0900

    Fix responsepropertyquery to actually return a property

--- data/src/main/java/com/atlantaai/landlord/data/api/ResponsePropertyQuery.kt
+++ data/src/main/java/com/atlantaai/landlord/data/api/ResponsePropertyQuery.kt
@@ -1,6 +1,8 @@
 package com.atlantaai.landlord.data.api
 
+import com.atlantaai.landlord.data.Property
+
 data class ResponsePropertyQuery(
-    val results: List<PropertyQuery>,
+    val results: List<Property>,
     val totalCount: Int
 )
